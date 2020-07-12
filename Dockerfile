MoveAngel/One4uBot

Dockerfile: Migrate to new image
* For faster build.

Signed-off-by: zainarbani <zaintsyariev@gmail.com>

Dockerfile
@@ -1,86 +1,17 @@
-# We're using Alpine Edge
-FROM alpine:edge
+FROM zainarbani/dockerub:one4u-alpine
+
+RUN mkdir /One4uBot && chmod 777 /One4uBot
+ENV PATH="/One4uBot/bin:$PATH"
+WORKDIR /One4uBot
+
+RUN git clone https://github.com/MoveAngel/One4uBot -b sql-extended /One4uBot
 
 #
-# We have to uncomment Community repo for some packages
+# Copies session and config(if it exists)
 #
-RUN sed -e 's;^#http\(.*\)/edge/community;http\1/edge/community;g' -i /etc/apk/repositories
-RUN echo 'http://dl-cdn.alpinelinux.org/alpine/edge/testing' >> /etc/apk/repositories
+COPY ./sample_config.env ./userbot.session* ./config.env* /One4uBot/
 
 #
-# Installing Packages
+# Finalization
 #
-RUN apk add --no-cache=true --update \
-    coreutils \
-    bash \
-    build-base \
-    bzip2-dev \
-    curl \
-    figlet \
-    gcc \
-    g++ \
-    git \
-    aria2 \
-    util-linux \
-    libevent \
-    jpeg-dev \
-    libffi-dev \
-    libpq \
-    libwebp-dev \
-    libxml2 \
-    libxml2-dev \
-    libxslt-dev \
-    linux-headers \
-    musl \
-    neofetch \
-    openssl-dev \
-    postgresql \
-    postgresql-client \
-    postgresql-dev \
-    openssl \
-    pv \
-    jq \
-    wget \
-    python3 \
-    python3-dev \
-    readline-dev \
-    sqlite \
-    ffmpeg \
-    sqlite-dev \
-    sudo \
-    chromium \
-    chromium-chromedriver \
-    zlib-dev \
-    jpeg \
-    zip \
-    megatools \
-    nodejs \
-    freetype-dev
-
-
-RUN python3 -m ensurepip \
-    && pip3 install --upgrade pip setuptools \
-    && pip3 install wheel \
-    && rm -r /usr/lib/python*/ensurepip && \
-    if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi && \
-    if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /usr/bin/python; fi && \
-    rm -r /root/.cache
-
-#
-# Clone repo and prepare working directory
-#
-RUN git clone -b sql-extended https://github.com/MoveAngel/One4uBot /root/One4uBot
-RUN mkdir /root/One4uBot/bin/
-WORKDIR /root/One4uBot/
-
-#
-# Copies session and config (if it exists)
-#
-COPY ./sample_config.env ./userbot.session* ./config.env* /root/userbot/
-
-#
-# Install requirements
-#
-RUN pip3 install -r requirements.txt
-RUN rm /bin/sh && ln -s /bin/bash /bin/sh
 CMD ["python3","-m","userbot"]

yuk1yuk1/ProjectBish

Signed-off-by: aidilaryanto <aidilaryanto18@gmail.com>
Signed-off-by: BianSepang <bianconneri46@gmail.com>

Dockerfile
@@ -1,77 +1,17 @@
-# We're using Alpine Edge
-FROM alpine:edge
+FROM zainarbani/dockerub:one4u-alpine
+
+RUN mkdir /ProjectBish && chmod 777 /ProjectBish
+ENV PATH="/ProjectBish/bin:$PATH"
+WORKDIR /ProjectBish
+
+RUN git clone https://github.com/BianSepang/ProjectBish -b experimentals-pb /ProjectBish
 
 #
-# We have to uncomment Community repo for some packages
+# Copies session and config(if it exists)
 #
-RUN sed -e 's;^#http\(.*\)/edge/community;http\1/edge/community;g' -i /etc/apk/repositories
+COPY ./sample_config.env ./userbot.session* ./config.env* /ProjectBish/
 
 #
-# Installing Packages
+# Finalization
 #
-RUN apk add --no-cache=true --update \
-    coreutils \
-    bash \
-    build-base \
-    bzip2-dev \
-    curl \
-    figlet \
-    gcc \
-    g++ \
-    git \
-    aria2 \
-    util-linux \
-    libevent \
-    jpeg-dev \
-    libffi-dev \
-    libpq \
-    libwebp-dev \
-    libxml2 \
-    libxml2-dev \
-    libxslt-dev \
-    linux-headers \
-    musl \
-    neofetch \
-    openssl-dev \
-    postgresql \
-    postgresql-client \
-    postgresql-dev \
-    openssl \
-    pv \
-    jq \
-    wget \
-    python3 \
-    python3-dev \
-    readline-dev \
-    sqlite \
-    ffmpeg \
-    sqlite-dev \
-    sudo \
-    chromium \
-    chromium-chromedriver \
-    zlib-dev \
-    jpeg \
-    zip \
-    freetype-dev
-
-RUN python3 -m ensurepip \
-    && pip3 install --upgrade pip setuptools \
-    && pip3 install wheel \
-    && rm -r /usr/lib/python*/ensurepip && \
-    if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi && \
-    if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /usr/bin/python; fi && \
-    rm -r /root/.cache
-
-#
-# Clone repo and prepare working directory
-#
-RUN git clone -b experimentals-pb https://github.com/BianSepang/ProjectBish /home/projectbish/
-RUN mkdir /home/projectbish/bin/
-WORKDIR /home/projectbish/
-
-#
-# Install requirements
-#
-RUN pip3 install -r requirements.txt
-RUN rm /bin/sh && ln -s /bin/bash /bin/sh
 CMD ["python3","-m","userbot"]