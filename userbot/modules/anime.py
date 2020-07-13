fed301620d6980bae2eb5fa2e86a7d1b3c7cca56
BianSepang <bianconneri46@gmail.com>
2020-07-14 03:22:41 +0800

    anime: rework the help msg
    Signed-off-by: BianSepang <bianconneri46@gmail.com>

--- userbot/modules/anime.py
+++ userbot/modules/anime.py
@@ -661,21 +661,21 @@ def is_gif(file):
 
 CMD_HELP.update({
     "anime":
-    ".anime <anime>\
-    \nUsage: Returns with Anime information.\
-    \n\n.manga <manga name>\
-    \nUsage: Returns with the Manga information.\
-    \n\n.akaizoku or .akayo <anime name>\
-    \nUsage: Returns with the Anime Download link.\
-    \n\n.char <character name>\
-    \nUsage: Return with character information.\
-    \n\n.upcoming\
-    \nUsage: Returns with Upcoming Anime information.\
-    \n\n.scanime <anime> or .sanime <anime>\
-    \nUsage: Search anime.\
-    \n\n.smanga <manga>\
-    \nUsage: Search manga.\
-    \n\n.whatanime Reply with media.\
-    \nUsage: Find anime from media file."
+    ">`.anime` **<anime>**"
+    "\nUsage: Returns with Anime information."
+    "\n\n>`.manga` **<manga name>**"
+    "\nUsage: Returns with the Manga information."
+    "\n\n>`.akaizoku` or `.akayo` **<anime name>**"
+    "\nUsage: Returns with the Anime Download link."
+    "\n\n>`.char` **<character name>**"
+    "\nUsage: Return with character information."
+    "\n\n>`.upcoming`"
+    "\nUsage: Returns with Upcoming Anime information."
+    "\n\n>`.scanime` **<anime>** or `.sanime` **<anime>**"
+    "\nUsage: Search anime."
+    "\n\n>`.smanga` **<manga>**"
+    "\nUsage: Search manga."
+    "\n\n>`.whatanime` **Reply to a Picture of an Anime Scene.**"
+    "\nUsage: Find anime from media file."
 })
 
