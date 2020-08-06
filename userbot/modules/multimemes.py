bcb1ee3f9e19d727e7521cc2ae3e4b52d67da211
KenHV <kenhv@null.net>
2020-08-06 17:13:27 +0530

    multimemes: don't crash if bot is not responding - Memify Bot - Deepfryer Bot
    Signed-off-by: KenHV <kenhv@null.net>
    Signed-off-by: BianSepang <bianconneri46@gmail.com>

--- userbot/modules/multimemes.py
+++ userbot/modules/multimemes.py
@@ -41,89 +41,92 @@ async def mim(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        await event.edit("`Syntax: reply to an image with .mmf` 'text on top' ; 'text on bottom' ")
+        await event.edit(
+            "`Syntax: reply to an image with .mms` 'text on top' ; 'text on bottom' "
+        )
         return
     reply_message = await event.get_reply_message()
     if not reply_message.media:
-        await event.edit("```reply to a image/sticker/gif```")
+        await event.edit("`Reply to a image/sticker/gif.`")
         return
-    chat = "@MemeAutobot"
     reply_message.sender
     await bot.download_file(reply_message.media)
     if reply_message.sender.bot:
-        await event.edit("```Reply to actual users message.```")
+        await event.edit("`Reply to actual users message.`")
         return
     else:
-        await event.edit("```Transfiguration Time! Mwahaha Memifying this image! (」ﾟﾛﾟ)｣ ```")
+        await event.edit("`Processing...`")
         await asyncio.sleep(5)
-
-    async with bot.conversation("@MemeAutobot") as bot_conv:
-        try:
-            memeVar = event.pattern_match.group(1)
-            await silently_send_message(bot_conv, "/start")
-            await asyncio.sleep(1)
-            await silently_send_message(bot_conv, memeVar)
-            await bot.send_file(chat, reply_message.media)
-            response = await bot_conv.get_response()
-        except YouBlockedUserError:
-            await event.reply("```Please unblock @MemeAutobot and try again```")
-            return
-        if response.text.startswith("Forward"):
-            await event.edit("```can you kindly disable your forward privacy settings for good, Nibba?```")
-        if "Okay..." in response.text:
-            await event.edit("```🛑 🤨 NANI?! This is not an image! This will take sum tym to convert to image... UwU 🧐 🛑```")
-            thumb = None
-            if os.path.exists(THUMB_IMAGE_PATH):
-                thumb = THUMB_IMAGE_PATH
-            input_str = event.pattern_match.group(1)
-            if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
-                os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
-            if event.reply_to_msg_id:
-                file_name = "meme.png"
-                reply_message = await event.get_reply_message()
-                to_download_directory = TEMP_DOWNLOAD_DIRECTORY
-                downloaded_file_name = os.path.join(
-                    to_download_directory, file_name)
-                downloaded_file_name = await bot.download_media(
-                    reply_message,
-                    downloaded_file_name,
-                )
-                if os.path.exists(downloaded_file_name):
-                    await bot.send_file(
-                        chat,
+    try:
+        async with bot.conversation("@MemeAutobot") as bot_conv:
+            chat = "@MemeAutobot"
+            try:
+                memeVar = event.pattern_match.group(1)
+                await silently_send_message(bot_conv, "/start")
+                await asyncio.sleep(1)
+                await silently_send_message(bot_conv, memeVar)
+                await bot.send_file(chat, reply_message.media)
+                response = await bot_conv.get_response()
+            except YouBlockedUserError:
+                await event.reply("`Please unblock @MemeAutobot and try again.`")
+                return
+            if response.text.startswith("Forward"):
+                await event.edit(
+                    "`Add `@MemeAutobot` to your forward privacy settings.`")
+            if "Okay..." in response.text:
+                await event.edit("`Converting...`")
+                thumb = None
+                if os.path.exists(THUMB_IMAGE_PATH):
+                    thumb = THUMB_IMAGE_PATH
+                input_str = event.pattern_match.group(1)
+                if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
+                    os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
+                if event.reply_to_msg_id:
+                    file_name = "meme.png"
+                    reply_message = await event.get_reply_message()
+                    to_download_directory = TEMP_DOWNLOAD_DIRECTORY
+                    downloaded_file_name = os.path.join(to_download_directory,
+                                                        file_name)
+                    downloaded_file_name = await bot.download_media(
+                        reply_message,
                         downloaded_file_name,
-                        force_document=False,
-                        supports_streaming=False,
-                        allow_cache=False,
-                        thumb=thumb,
                     )
-                    os.remove(downloaded_file_name)
-                else:
-                    await event.edit("File Not Found {}".format(input_str))
-            response = await bot_conv.get_response()
-            the_download_directory = TEMP_DOWNLOAD_DIRECTORY
-            files_name = "memes.webp"
-            download_file_name = os.path.join(
-                the_download_directory, files_name)
-            await bot.download_media(
-                response.media,
-                download_file_name,
-            )
-            requires_file_name = TEMP_DOWNLOAD_DIRECTORY + "memes.webp"
-            await bot.send_file(  # pylint:disable=E0602
-                event.chat_id,
-                requires_file_name,
-                supports_streaming=False,
-                caption="Memifyed",
-            )
-            await event.delete()
-            # await bot.send_message(event.chat_id, "`☠️☠️Ah Shit... Here we go
-            # Again!🔥🔥`")
-        elif not is_message_image(reply_message):
-            await event.edit("Invalid message type. Plz choose right message type u NIBBA.")
-            return
-        else:
-            await bot.send_file(event.chat_id, response.media)
+                    if os.path.exists(downloaded_file_name):
+                        await bot.send_file(
+                            chat,
+                            downloaded_file_name,
+                            force_document=False,
+                            supports_streaming=False,
+                            allow_cache=False,
+                            thumb=thumb,
+                        )
+                        os.remove(downloaded_file_name)
+                    else:
+                        await event.edit("File Not Found {}".format(input_str))
+                response = await bot_conv.get_response()
+                the_download_directory = TEMP_DOWNLOAD_DIRECTORY
+                files_name = "memes.webp"
+                download_file_name = os.path.join(the_download_directory,
+                                                  files_name)
+                await bot.download_media(
+                    response.media,
+                    download_file_name,
+                )
+                requires_file_name = TEMP_DOWNLOAD_DIRECTORY + "memes.webp"
+                await bot.send_file(  # pylint:disable=E0602
+                    event.chat_id,
+                    requires_file_name,
+                    supports_streaming=False,
+                    caption="Memifyed",
+                )
+                await event.delete()
+            elif not is_message_image(reply_message):
+                await event.edit("`Invalid message type.`")
+                return
+            else:
+                await bot.send_file(event.chat_id, response.media)
+    except TimeoutError:
+        return await event.edit("**Error:** @MemeAutobot **is not responding.**")
 
 
 def is_message_image(message):
@@ -253,8 +256,8 @@ async def hazz(hazmat):
     return os.remove(downloaded_file_name)
 
 
-@register(outgoing=True, pattern=r'^\.df(:? |$)([1-8])?')
-async def fryerrr(fry):
+@register(outgoing=True, pattern=r"^\.df(:? |$)([1-8])?")
+async def _(fry):
     await fry.edit("`Sending information...`")
     level = fry.pattern_match.group(2)
     if fry.fwd_from:
@@ -271,46 +274,40 @@ async def fryerrr(fry):
         return
     chat = "@image_deepfrybot"
     message_id_to_reply = fry.message.reply_to_msg_id
-    async with fry.client.conversation(chat) as conv:
-        try:
-            msg = await conv.send_message(reply_message)
-            if level:
-                m = f"/deepfry {level}"
-                msg_level = await conv.send_message(
-                    m,
-                    reply_to=msg.id)
-                r = await conv.get_response()
-                response = await conv.get_response()
-            else:
-                response = await conv.get_response()
-            """ - don't spam notif - """
-            await bot.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await fry.reply("`Please unblock` @image_deepfrybot`...`")
-            return
-        if response.text.startswith("Forward"):
-            await fry.edit("`Please disable your forward privacy setting...`")
-        else:
-            downloaded_file_name = await fry.client.download_media(
-                response.media,
-                TEMP_DOWNLOAD_DIRECTORY
-            )
-            await fry.client.send_file(
-                fry.chat_id,
-                downloaded_file_name,
-                force_document=False,
-                reply_to=message_id_to_reply
-            )
-            """ - cleanup chat after completed - """
+    try:
+        async with fry.client.conversation(chat) as conv:
             try:
-                msg_level
-            except NameError:
-                await fry.client.delete_messages(conv.chat_id,
-                                                 [msg.id, response.id])
+                msg = await conv.send_message(reply_message)
+                if level:
+                    m = f"/deepfry {level}"
+                    msg_level = await conv.send_message(m, reply_to=msg.id)
+                    r = await conv.get_response()
+                response = await conv.get_response()
+                """ - don't spam notif - """
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                await fry.reply("`Please unblock` @image_deepfrybot`...`")
+                return
+            if response.text.startswith("Forward"):
+                await fry.edit("`Please disable your forward privacy setting...`")
             else:
-                await fry.client.delete_messages(
-                    conv.chat_id,
-                    [msg.id, response.id, r.id, msg_level.id])
+                downloaded_file_name = await fry.client.download_media(
+                    response.media, TEMP_DOWNLOAD_DIRECTORY)
+                await fry.client.send_file(fry.chat_id,
+                                           downloaded_file_name,
+                                           force_document=False,
+                                           reply_to=message_id_to_reply)
+                """ - cleanup chat after completed - """
+                try:
+                    msg_level
+                except NameError:
+                    await fry.client.delete_messages(conv.chat_id,
+                                                     [msg.id, response.id])
+                else:
+                    await fry.client.delete_messages(
+                        conv.chat_id, [msg.id, response.id, r.id, msg_level.id])
+    except TimeoutError:
+        return await fry.edit("**Error:** @image_deepfrybot **is not responding.**")
     await fry.delete()
     return os.remove(downloaded_file_name)
 
